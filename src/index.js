f9cf75f5f031d027b079dd7b1ef28b220bdc0650
Luiz Felipe das Neves Lopes <luizfelipe2310@gmail.com>
2019-03-13 17:12:01 -0300

    componet

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const number = require('./modulos/number'); // get number
+
+const express = require('express');
+const app = express();
+
+app.get('/v1/phone/:number', (req, res) => {
+  number.getphone(req.params.number)
+    .then(info => res.send(info))
+    .catch(err => res.send(err))
+});
+
+app.listen((process.env.PORT || 5000));
+console.log(`Started on port: ${process.env.PORT || 5000}`);
+exports = module.exports = app;
---
+++ src/modulos/number.js
@@ -0,0 +1,40 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+function getoptions(number) {
+  const options = {
+    uri: 'https://phonenumber-lookup.info',
+    body: `phone=${number}`,
+    method: 'POST',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    transform(body) { 
+      return cheerio.load(body);
+    },
+  };
+  return options;
+}
+module.exports = {
+  getphone: function(number) {
+      const options = getoptions(number);
+      return rp(options)
+        .then(($) => {
+          const type = $('tr:nth-of-type(2) td:nth-of-type(2)').text();
+          const location = $('tr:nth-of-type(5) td:nth-of-type(2)').text();
+          const contrycode = $('tr:nth-of-type(6) td:nth-of-type(2)').text();
+          const countryname = $('tr:nth-of-type(7) td:nth-of-type(2)').text();
+          const contrynetwork = $('tr:nth-of-type(8) td:nth-of-type(2)').text();
+          const info = {
+            number,
+            type,
+            location,
+            contrycode, 
+            countryname, 
+            contrynetwork,
+          };
+          return(info);
+        });
+  }
+}
